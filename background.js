28603d6c95d3e6d5eb5d470c56d499308aecfe6d
jfferreiraMD <jose97df@gmail.com>
2024-11-13 23:44:31 +0000

    Fix expected urls, listening onBeforeRequest

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
+console.log("service worker working");
+
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        console.log(details)
+        if (details.requestBody && details.requestBody.raw) {
+            const raw = details.requestBody.raw[0].bytes;
+            const text = new TextDecoder().decode(raw);
+            console.log("Decoded body:", text);
+        }
     },
-    { urls: ["*://*.anthropic.com/*/messages/*"] },
+    { urls: ["https://*.claude.ai/api/*/chat_conversations/*/completion"] },
     ["requestBody"]
 );
+
